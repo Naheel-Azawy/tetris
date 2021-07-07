c63e42a22a58b71982f8f99befd85071bfe08aba
Naheel-Azawy <naheelazawy@gmail.com>
2021-07-08 01:51:59 +0300

    kc144

--- Makefile
+++ Makefile
@@ -1,18 +1,22 @@
 PREFIX = /usr/bin/
 CC = gcc
 
-LICENSE  = LICENSE_HEADER
-SRC      = tetris.c
-HDR      = tetris.h
-SRC_TUI  = tetris_tui.c
-SRC_INO  = tetris_arduino.ino
-SRC_WASM = tetris_wasm.c
+LICENSE   = LICENSE_HEADER
+SRC       = tetris.c
+HDR       = tetris.h
+SRC_TUI   = tetris_tui.c
+SRC_INO   = tetris_arduino.ino
+SRC_KC144 = tetris_keychainino_v4_1.ino
+SRC_WASM  = tetris_wasm.c
 
-all:     tui tui-all ino web
+all:     tui
+allall:  tui ino kc144 web
+
 tui:     build/tetris
 tui-all: build/tetris-all.c
-ino:     build/tetris.ino
-web:     index.html
+ino:     build/$(SRC_INO)
+kc144:   build/$(SRC_KC144)
+web:     build/index.html
 
 build/tetris: $(SRC) $(HDR) $(SRC_TUI)
 	mkdir -p build
@@ -23,24 +27,29 @@ build/tetris-all.c: $(SRC) $(HDR) $(SRC_TUI)
 	cat $(LICENSE) $(HDR) $(SRC) $(SRC_TUI) > ./build/tetris-all.c
 	sed -i 's/#include "tetris.h"//' ./build/tetris-all.c
 
-./build/tetris_wasm.wasm: $(SRC) $(HDR) $(SRC_WASM)
+build/tetris_wasm.wasm: $(SRC) $(HDR) $(SRC_WASM)
 	mkdir -p build
 	cat $(LICENSE) $(HDR) $(SRC) $(SRC_WASM) > ./build/tetris_wasm_all.c
 	sed -i 's/#include "tetris.h"//' ./build/tetris_wasm_all.c
 	emcc -Os -s STANDALONE_WASM -s EXPORTED_FUNCTIONS="['_setup', '_loop', '_get_data_len']" -Wl,--no-entry ./build/tetris_wasm_all.c -o ./build/tetris_wasm.wasm
 
-index.html: ./build/tetris_wasm.wasm tetris_wasm_ui.js tetris_wasm_ui.html
+build/index.html: build/tetris_wasm.wasm tetris_wasm_ui.js tetris_wasm_ui.html
 	cat $(LICENSE)                    > ./build/tetris_wasm_all.js
 	echo 'const tetris_asm = atob(`' >> ./build/tetris_wasm_all.js
 	base64 ./build/tetris_wasm.wasm  >> ./build/tetris_wasm_all.js
 	echo '`);'                       >> ./build/tetris_wasm_all.js
 	cat tetris_wasm_ui.js            >> ./build/tetris_wasm_all.js
-	sed -e '/SCRIPT/ {r ./build/tetris_wasm_all.js' -e 'd}' tetris_wasm_ui.html > index.html
+	sed -e '/SCRIPT/ {r ./build/tetris_wasm_all.js' -e 'd}' tetris_wasm_ui.html > build/index.html
 
-build/tetris.ino: $(SRC) $(HDR) $(SRC_INO)
+build/$(SRC_INO): $(SRC) $(HDR) $(SRC_INO)
 	mkdir -p build
-	cat $(LICENSE) $(HDR) $(SRC) $(SRC_INO) > ./build/tetris.ino
-	sed -i 's/#include "tetris.h"//' ./build/tetris.ino
+	cat $(LICENSE) $(HDR) $(SRC) $(SRC_INO) > ./build/$(SRC_INO)
+	sed -i 's/#include "tetris.h"//' ./build/$(SRC_INO)
+
+build/$(SRC_KC144): $(SRC) $(HDR) $(SRC_KC144)
+	mkdir -p build
+	cat $(LICENSE) $(HDR) $(SRC) $(SRC_KC144) > ./build/$(SRC_KC144)
+	sed -i 's/#include "tetris.h"//' ./build/$(SRC_KC144)
 
 clean:
 	rm -rf ./build
@@ -51,3 +60,5 @@ install:
 
 uninstall:
 	rm -f $(DESTDIR)$(PREFIX)/tetris
+
+.PHONY: install uninstall clean
---
+++ tetris_keychainino_v4_1.ino
@@ -0,0 +1,131 @@
+#include <KeyChainino.h>
+
+// timer counter
+unsigned long timer1_count_;
+
+// game main variable
+game tetris;
+
+// unset when sleeping
+bool game_started = true;
+
+// used for buttons long press in check_btns()
+bool prev_clicked = false;
+
+ISR(TIMER1_OVF_vect) {
+  // disable Timer1 interrupts
+  TIMSK1 &= ~(1 << TOIE1);
+  TCNT1 = timer1_count_;  // load counter for next interrupt
+  TIMSK1 |= (1 << TOIE1); // enable timer overflow interrupt
+}
+
+ISR(INT1_vect) { // BUTTON_A INTERRUPT
+  // do nothing
+}
+
+ISR(INT0_vect) { // BUTTON_B INTERRUPT
+  // do nothing
+}
+
+int rand_int() {
+  return random(0, 100);
+}
+
+void rand_seed() {
+  randomSeed(analogRead(A0));
+}
+
+void check_btns() {
+  if (!digitalRead(BUTTON_A) || !digitalRead(BUTTON_B)) {
+    // any button is pressed
+
+    if (prev_clicked) {
+      // previously clicked once
+      delay(2);
+    } else {
+      // just clicked
+      delay(80);
+    }
+
+    if (!digitalRead(BUTTON_A) && !digitalRead(BUTTON_B)) {
+      // both buttons clicked
+      tetris.msg = ROTATE1;
+      delay(80);
+    } else {
+      if (!digitalRead(BUTTON_A)) {
+        // A is clicked
+        tetris.msg = LEFT;
+      } else if (!digitalRead(BUTTON_B)) {
+        // B is clicked
+        tetris.msg = RIGHT;
+      }
+      prev_clicked = true;
+    }
+
+  } else {
+    // nothing is clicked
+    prev_clicked = false;
+  }
+}
+
+void draw(game *g) {
+  for (int j = T; j < g->h + T; ++j) {
+    check_btns();
+    for (int i = 0; i < g->w; ++i) {
+      KC.pixel(i, j - T, CELL(g->boardtmp, i, j));
+    }
+  }
+  KC.display();
+}
+
+void draw_game_over(game *g) {
+  KC.clear();
+  KC.display();
+  static char buf[24];
+  sprintf(buf, "GAME OVER %d", g->score);
+  KC.scrollText(buf, 1);
+  delay(4000);
+  KC.clear();
+  KC.display();
+  game_started = false;
+}
+
+void setup() {
+  // initialize and clear display
+  KC.init();
+  KC.clear();
+  KC.display();
+
+  // configure buttons pins
+  pinMode(BUTTON_A, INPUT_PULLUP);
+  pinMode(BUTTON_B, INPUT_PULLUP);
+
+  // initialize Timer 1
+  timer1_count_ = 64900;
+  noInterrupts();          // disable interrupts
+  TCCR1A = 0;              // normal operation
+  TCCR1B = 0;              // clear prescaler
+  TCNT1 = timer1_count_;   // load counter
+  TCCR1B |= (1 << CS10);   // prescaler = 1024
+  TIMSK1 |= (1 << TOIE1);  // enable timer overflow
+  interrupts();            // enable all interrupts
+
+  // initialize the game
+  tetris.w = 12;
+  tetris.h = 12;
+  game_init(&tetris);
+  game_started = true;
+
+  KC.goSleep();
+}
+
+void loop() {
+  if (game_started) {
+    check_btns();
+    game_loop(&tetris);
+  } else {
+    KC.goSleep(); // sleep to reduce power
+    game_init(&tetris);
+    game_started = true;
+  }
+}
